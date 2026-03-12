bc07b2e18749b16d0cf4d9b924c0faf6bc8482ff
Guillermo <g.lindez.alonso@gmail.com>
2026-03-12 19:28:24 +0100

    creación repositorio

---
+++ matriz.py
@@ -0,0 +1,17 @@
+class Matriz:
+    def __init__(self, dim):
+        self.dim: int = dim
+
+    def __str__(self):
+        a = [[0] * 3] * 3
+        for i in range(len(a)):
+            print(a[i])
+
+
+
+
+
+# Manera de crear e imprimir la matriz de forma correcta:
+# a = [[0] * 3] * 3
+# for i in range(len(a)):
+#     print(a[i])
