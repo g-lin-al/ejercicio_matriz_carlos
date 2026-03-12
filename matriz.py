9867777e1d862435b2ab78a340c5f684589e71a7
g-lin-al <g.lindez.alonso@gmail.com>
2026-03-12 20:58:23 +0100

    Definida clase Matriz y método para matrices aleatorias

--- matriz.py
+++ matriz.py
@@ -1,12 +1,33 @@
+import random
+
+
 class Matriz:
     def __init__(self, dim):
+        self.INI: int = 0
+        self.MIN_RAND: int = 1
+        self.MAX_RAND: int = 9
         self.dim: int = dim
+        self.matriz: list[list[int]] = [[self.INI] * self.dim for _ in range(self.dim)]
 
-    def __str__(self):
-        a = [[0] * 3] * 3
-        for i in range(len(a)):
-            print(a[i])
+    # def __str__(self): # dunder str necesita un return y no sé cómo hacerlo con este tipo
+    #     for i in range(len(self.matriz)):
+    #         print(self.matriz[i])
 
+    def imprimir(self):
+        for i in range(len(self.matriz)):
+            print(self.matriz[i])
+
+    def rellenar_aleatorio(self):
+        for i in range(self.dim):
+            for j in range(self.dim):
+                self.matriz[i][j] = random.randint(self.MIN_RAND, self.MAX_RAND)
+
+
+a: Matriz = Matriz(3)
+a.imprimir()
+a.rellenar_aleatorio()
+a.imprimir()
+print(a.matriz)
 
 
 
